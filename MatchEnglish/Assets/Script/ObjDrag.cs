a1d233f2d2dc4aea9a27811ede2a0e89d3448c34
argojuni <juniargoprasaja05@gmail.com>
2023-12-11 10:13:48 +0700

    Mencocokan text dengan gambar

--- MatchEnglish/Assets/Script/ObjDrag.cs
+++ MatchEnglish/Assets/Script/ObjDrag.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class ObjDrag : MonoBehaviour
 {
-    public Vector2 SavePosisi;
+
+    Vector2 SavePosisi;
+    public bool IsDiatasObj;
+
+    Transform saveObj;
+
+    public int ID;
 
     private void Start()
     {
@@ -17,11 +23,53 @@ public class ObjDrag : MonoBehaviour
     }
     private void OnMouseUp()
     {
-        transform.position = SavePosisi;
+        if (IsDiatasObj)
+        {
+            int ID_TempatDrop = saveObj.GetComponent<tempat_drop>().ID;
+
+            if(ID == ID_TempatDrop)
+            {
+                transform.SetParent(saveObj);
+                transform.localPosition = Vector3.zero;
+                transform.localScale = new Vector3(0.35f, 0.2f);
+                //transform.localScale = saveObj.localScale;
+
+                saveObj.GetComponent<SpriteRenderer>().enabled = false;
+                saveObj.GetComponent<Rigidbody2D>().simulated = false;
+                saveObj.GetComponent<BoxCollider2D>().enabled = false;
+
+                gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            }
+            else
+            {
+                transform.position = SavePosisi;
+            }
+        }
+        else
+        {
+            transform.position = SavePosisi;
+        }
     }
     private void OnMouseDrag()
     {
         Vector2 Pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = Pos;
+    }
+
+    private void OnTriggerStay2D(Collider2D trig)
+    {
+        if (trig.CompareTag("Drop"))
+        {
+            IsDiatasObj = true;
+            saveObj = trig.gameObject.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D trig)
+    {
+        if (trig.CompareTag("Drop"))
+        {
+            IsDiatasObj = false;
+        }
     }
 }
